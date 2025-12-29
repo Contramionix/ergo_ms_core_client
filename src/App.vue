0da5a685c191ffb7c469353ca373efad90268d49
kotomafia <retrosmith2@gmail.com>
2025-12-29 10:15:43 +0300

    Revert "Merge commit '69c3dc0dc05d6c6204fbe022ee26e9eb7ffccae1' into dev"
    This reverts commit e88c3ef7d55126ab9de79e6807efc3d047027432, reversing
    changes made to 034d1c1ab432b9206dea3fb967824944307651e6.

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@ import { useRoute, useRouter } from 'vue-router'
 import LayoutMenu from '@/LayoutMenu.vue'
 import LayoutStart from '@/LayoutStart.vue'
 import LayoutPublic from '@/LayoutPublic.vue'
-import LayoutWindows from '@/LayoutWindows.vue'
 import NotificationProvider from '@/components/NotificationProvider.vue'
 
 const route = useRoute()
@@ -16,30 +15,11 @@ router.isReady().then(() => {
   isReady.value = true
 })
 
-// Проверяем, использовать ли новый UI с окнами
-// Можно переключать через query параметр ?ui=windows или localStorage
-const useWindowsUI = computed(() => {
-  // Проверяем query параметр
-  if (route.query.ui === 'windows') {
-    return true
-  }
-  if (route.query.ui === 'menu') {
-    return false
-  }
-  // Проверяем localStorage (для сохранения выбора пользователя)
-  const savedUI = localStorage.getItem('ergo_ms_ui_mode')
-  return savedUI === 'windows'
-})
-
 const currentLayout = computed(() => {
   if (route.meta && route.meta.startRoute === true) {
     return LayoutStart
   }
-  if (route.meta && route.meta.public === true) {
-    return LayoutPublic
-  }
-  // Используем новый UI с окнами, если включен
-  return useWindowsUI.value ? LayoutWindows : LayoutMenu
+  return route.meta && route.meta.public === true ? LayoutPublic : LayoutMenu
 })
 </script>
 
